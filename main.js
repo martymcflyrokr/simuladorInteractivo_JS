810f5c2d8cacdfee2883e72b2e7f32cdfdcfc9c8
Mauro Mena <mauromenae@gmail.com>
2022-02-16 02:26:49 -0300

    Nuevos cambios

--- main.js
+++ main.js
@@ -27,9 +27,9 @@ function consultaCantidad(caneleg, stockini, usuario, tipobur) {
 
 function confirmaCompra(stockac, stockini, caneleg, tipobur, precio) {
     stockac = stockini - caneleg
-    console.log('El stock actual es de ' + stockac + ' hamburgesas tipo ' + tipobur)
     alert('Compraste ' + caneleg + ' de hamburgesas tipo ' + tipobur + '\npor un total de ' + caneleg * precio + ' ARS')
     console.log('El user gastó ' + caneleg * precio + ' ARS ')
+    console.log('El stock actual es de ' + stockac + ' hamburgesas tipo ' + tipobur)
 }
 
 let cliente = prompt('Ingrese su nombre')
@@ -42,7 +42,7 @@ for (i = 1; i < 4; i++) {
     let cantidadElegidaPollo
     let stockActualVacuna
     let cantidadElegidaVacuna
-
+    
     let burgerElegida = (
         prompt('ingrese el tipo de hamburgesa que quiere llevar: \n Vegana \n Pollo \n Vacuna'))
 
@@ -50,19 +50,18 @@ for (i = 1; i < 4; i++) {
         console.log('El usuario eligió hamburgesa tipo ' + burgerElegida)
 
         if (burgerElegida == 'Vegana') {
+            
             cantidadElegidaVegana = (prompt('Indique la cantidad que desea comprar.\nContamos con un stock de ' + stockInicialVegana + ' unidades'))
             console.log('El usuario ' + cliente + ' quiere comprar ' + cantidadElegidaVegana + ' de burgers tipo ' + burgerVegana)
 
             if (cantidadElegidaVegana <= stockInicialVegana) {
                 confirmaCompra(stockActualVegana, stockInicialVegana, cantidadElegidaVegana, burgerVegana, precioBurgerVegana)
                 stockInicialVegana -= cantidadElegidaVegana
-
+                totalVegana = cantidadElegidaVegana*precioBurgerVegana
             } else {
                 excedeStock()
             }
-
         }
-
         else if (burgerElegida == 'Vacuna') {
             cantidadElegidaVacuna = (prompt('Indique la cantidad que desea comprar.\nContamos con un stock de ' + stockInicialVacuna + ' unidades'))
             console.log('El usuario ' + cliente + ' quiere comprar ' + cantidadElegidaVacuna + ' de burgers tipo ' + burgerVacuna)
@@ -70,31 +69,31 @@ for (i = 1; i < 4; i++) {
             if (cantidadElegidaVacuna <= stockInicialVacuna) {
                 confirmaCompra(stockActualVacuna, stockInicialVacuna, cantidadElegidaVacuna, burgerVacuna, precioBurgerVacuna)
                 stockInicialVacuna -= cantidadElegidaVacuna
+
             } else {
                 excedeStock()
             }
-
         }
-
         else if (burgerElegida == 'Pollo') {
             cantidadElegidaPollo = (prompt('Indique la cantidad que desea comprar.\nContamos con un stock de ' + stockInicialPollo + ' unidades'))
             console.log('El usuario ' + cliente + ' quiere comprar ' + cantidadElegidaPollo + ' de burgers tipo ' + burgerPollo)
 
             if (cantidadElegidaPollo <= stockInicialPollo) {
                 confirmaCompra(stockActualPollo, stockInicialPollo, cantidadElegidaPollo, burgerPollo, precioBurgerPollo)
-                stockInicialPollo -= cantidadElegidaPollo
+                stockInicialPollo -= cantidadElegidaPollo   
             } else {
                 excedeStock()
             }
-
         }
-
         else {
             console.log('El usuario enloqueció')
         }
-        
+
     } else {
         seleccionInvalida()
     }
+
 }
+
+
 
