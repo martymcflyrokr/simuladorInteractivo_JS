fe06fba92f3955ae642a261de5b10b713e77891c
Mauro Mena <mauromenae@gmail.com>
2022-02-20 02:20:07 -0300

    indentando 2.0

--- main2.js
+++ main2.js
@@ -11,84 +11,84 @@ let burgerVacuna = 'Vacuna'
 let stockVacuna = 8
 let precioBurgerVacuna = 800
 
-let unidadesTotal=0
+let unidadesTotal = 0
 let stockNuevo
-let unidadesCompradas=0
+let unidadesCompradas = 0
 let precioCompra
-let precioTotal=0
+let precioTotal = 0
 
 //Funciones
 function pedirCantidad(stock) {
-    console.log ('El stock actual es ' + stock)
+    console.log('El stock actual es ' + stock)
     unidadesCompradas = parseInt(prompt('Cuantas unidades desea comprar?'))
     console.log('quiere comprar ' + unidadesCompradas)
-        return unidadesCompradas
+    return unidadesCompradas
 }
 
 function actualizarstock(stocktipo) {
     console.log('usuario compró : ' + unidadesCompradas + ' unidades')
-    stockNuevo = stocktipo-unidadesCompradas
+    stockNuevo = stocktipo - unidadesCompradas
     stocktipo = stockNuevo
-        return stockNuevo
+    return stockNuevo
 }
 
 function actualizarUnidades() {
     unidadesTotal = unidadesTotal + unidadesCompradas
-        return unidadesTotal
+    return unidadesTotal
 }
-function sumarPrecio (precio) {
+function sumarPrecio(precio) {
     precioCompra = unidadesCompradas * precio
     precioTotal = precioCompra + precioTotal
-        return precioTotal
+    return precioTotal
 }
 function stockInsuficiente(stock) {
     alert('No contamos con la cantidad que indica.\nSolo contamos con ' + stock + ' unidades.')
-    console.log('El usuario eligió mas que el stock actual')   
+    console.log('El usuario eligió mas que el stock actual')
 }
 function resumenCompra(unidadestotales, preciototal) {
-    console.log('Resumen compra total : \n - - - - - - - - - \n'+
-    'Burgers Vacuna - ' + unidadestotales + ' unidades: $' + preciototal)
+    console.log('Resumen compra total : \n - - - - - - - - - \n' +
+        'Burgers Vacuna - ' + unidadestotales + ' unidades: $' + preciototal)
 }
 
 let cliente = prompt('Ingrese nombre:')
 let consultaCompra = prompt('Quiere realizar una compra?\nsi \nno')
 
-while ( consultaCompra === 'si') {
+while (consultaCompra === 'si') {
 
     seleccionBurger = prompt('Seleccione el tipo de hamburguesa:\n1- Vacuna \n2- Pollo \n3- Vegana')
-    
-    
+
+
 
     switch (seleccionBurger) {
         case 'Vacuna':
             pedirCantidad(stockVacuna)
-            if(unidadesCompradas<=stockVacuna) {
+            if (unidadesCompradas <= stockVacuna) {
                 actualizarstock(stockVacuna)
                 // stockVacuna = stockNuevo
                 actualizarUnidades()
                 sumarPrecio(precioBurgerVacuna)
-            }else{
+            } else {
                 stockInsuficiente(stockVacuna)
             }
 
             break
-        
+
         case 'Pollo':
-            
-            break
-        
-        case 'Vegana': 
 
             break
-        
+
+        case 'Vegana':
+
+            break
+
         default: alert('seleccionó un tipo de hamburgesa invalido')
     }
 
     let pedirResumen = prompt('Desea ver el resumen de su compra?')
     if (pedirResumen === 'si') {
         resumenCompra(unidadesTotal, precioTotal)
-        
-    }else { 
+
+    } else {
         console.log('usuario no quiere ver el resumen')
     }
 
